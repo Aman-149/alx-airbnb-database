2f2e3c5b983dc04b46974b86aebe6d6e1961649d
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-08 23:03:49 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,18 @@
+-- subqueries.sql
+
+-- 1) Non-correlated subquery: properties with avg rating > 4.0
+SELECT property_id, name
+FROM Property
+WHERE property_id IN (
+  SELECT property_id
+  FROM Review
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+-- 2) Correlated subquery: users who have made more than 3 bookings
+SELECT u.user_id, u.first_name, u.last_name,
+  (SELECT COUNT(*) FROM Booking b WHERE b.user_id = u.user_id) AS bookings_count
+FROM `User` u
+WHERE (SELECT COUNT(*) FROM Booking b WHERE b.user_id = u.user_id) > 3;
 
